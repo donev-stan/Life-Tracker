c0e3948daaa4a805f13d0a0046c761dc2b8cc67f
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-02-16 22:44:44 +0200

    Separate Prime Theme Module for imports

--- life-tracker-app/src/app/app.module.ts
+++ life-tracker-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PrimeThemeModule } from './shared/prime/prime-theme.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, PrimeThemeModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
